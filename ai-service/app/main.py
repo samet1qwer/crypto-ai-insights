a98b48f8f487ae82601837ded4069cc6c92cdb62
CyberTempest1w <samet102a@gmail.com>
2026-01-28 18:27:22 +0300

    add main.py

--- ai-service/app/main.py
+++ ai-service/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from app.api import predict
+from api.predict import router as predict_router
+from api.health import router as health_router
 
 app = FastAPI(title="Crypto AI Service")
 
-app.include_router(predict.router)
+app.include_router(health_router)
+app.include_router(predict_router)

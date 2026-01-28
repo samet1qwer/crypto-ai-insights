8196f76fc72301ce07362f5218e0bd951d42beca
CyberTempest1w <samet102a@gmail.com>
2026-01-28 22:27:37 +0300

    add new indicators

--- ai-service/app/utils/indicators.py
+++ ai-service/app/utils/indicators.py
@@ -1,6 +1,22 @@
 import pandas as pd
-import ta
 
-def add_indicators(df: pd.DataFrame) -> pd.DataFrame:
-    df["rsi"] = ta.momentum.RSIIndicator(df["price"], window=14).rsi()
+def calculate_rsi(series, period=14):
+    delta = series.diff()
+    gain = delta.clip(lower=0)
+    loss = -delta.clip(upper=0)
+
+    avg_gain = gain.rolling(period).mean()
+    avg_loss = loss.rolling(period).mean()
+
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
+
+def calculate_ema(series, span=20):
+    return series.ewm(span=span, adjust=False).mean()
+
+
+def add_indicators(df: pd.DataFrame):
+    df["rsi"] = calculate_rsi(df["close"])
+    df["ema"] = calculate_ema(df["close"])
     return df

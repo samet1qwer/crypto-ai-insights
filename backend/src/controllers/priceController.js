9a17c8a5901ecf20c7d7db337fb9ce968ac3569f
CyberTempest1w <samet102a@gmail.com>
2026-01-30 22:55:12 +0300

    add contollers price

---
+++ backend/src/controllers/priceController.js
@@ -0,0 +1,20 @@
+exports.getPrice = async (req, res) => {
+  const symbol = req.params.symbol;
+  const price = await crypto_price.findOne({ symbol: symbol });
+  if (!price) {
+    res.status(404).send("Price not found");
+  }
+  res.send(price);
+};
+
+exports.addPrice = async (req, res) => {
+  const crypto_id = req.body.crypto_id;
+  const price = req.body.price;
+
+  const newPrice = new crypto_price({
+    crypto_id: crypto_id,
+    price: price,
+  });
+  const savedPrice = await newPrice.save();
+  res.send(savedPrice);
+};
--- backend/src/routers/price.js
+++ backend/src/routers/price.js
@@ -1,27 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const crypto_price = require("../models/price_data");
+const { getPrice, addPrice } = require("../controllers/priceController");
 
-router.get("/price/:symbol", async (req, res) => {
-  const symbol = req.params.symbol;
-  const price = await crypto_price.findOne({ symbol: symbol });
-  if (!price) {
-    res.status(404).send("Price not found");
-  }
-  res.send(price);
-});
+router.get("/price/:symbol", getPrice);
 
-router.post("/price/add", async (req, res) => {
-  const crypto_id = req.body.crypto_id;
-  const price = req.body.price;
-
-  const newPrice = new crypto_price({
-    crypto_id: crypto_id,
-    price: price,
-  });
-
-  const savedPrice = await newPrice.save();
-  res.send(savedPrice);
-});
+router.post("/price/add", addPrice);
 
 module.exports = router;
